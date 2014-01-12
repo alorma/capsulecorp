ef1dce0da3548cad1fc6057c784d54eb884e7dab
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-01-12 18:14:24 +0100

    WIKI

--- Demo/src/main/java/cat/alorma/capsules/ui/fragment/TextColorsFragment.java
+++ Demo/src/main/java/cat/alorma/capsules/ui/fragment/TextColorsFragment.java
@@ -11,9 +11,9 @@ import cat.alorma.capsulecorp.library.capsule.impl.TextCapsule;
 public class TextColorsFragment extends BaseFragment {
     @Override
     protected void generateCapsules() {
-        setCapsule1(new TextCapsule("1", Color.parseColor("#000000"), Color.parseColor("#e67e22")));
-        setCapsule2(new TextCapsule("2", Color.parseColor("#000000"), Color.parseColor("#9b59b6")));
-        setCapsule3(new TextCapsule("3", Color.parseColor("#000000"), Color.parseColor("#bdc3c7")));
-        setCapsule4(new TextCapsule("4", Color.parseColor("#000000"), Color.parseColor("#1abc9c")));
+        setCapsule1(new TextCapsule("0", Color.parseColor("#000000"), Color.parseColor("#e67e22")));
+        setCapsule2(new TextCapsule("1", Color.parseColor("#000000"), Color.parseColor("#9b59b6")));
+        setCapsule3(new TextCapsule("2", Color.parseColor("#000000"), Color.parseColor("#bdc3c7")));
+        setCapsule4(new TextCapsule("3", Color.parseColor("#000000"), Color.parseColor("#1abc9c")));
     }
 }
--- doc/wiki/DISPENSER.md
+++ doc/wiki/DISPENSER.md
@@ -12,3 +12,32 @@
 ```
 
 DispenserView is a square view, it takes the big size  (Width or Height)
+
+In activity or fragment findView:
+
+``` java
+DispenserView dispenserView = (DispenserView) view.findViewById(R.id.dispenserView);
+```
+
+Then you are able to add capsules.
+
+``` java
+Capsule capsule = new ColorCapsule(Color.parseColor("#e67e22"));
+dispenserView.addCapsule(3, capsule);
+```
+
+DispenserView just admits postions from 0 to 3 (1 to 4).
+
+### Modes
+
+* **Single:** One capsule, square
+* **Two columns:** Two capsules, one column per capsule
+* **Column and squares: Three capsules
+    * Position 0 in a column
+    * Position 1 in right top square
+    * Position 2 in right bottom square
+* **Squares: Four capsules
+    * Position 0 in left top square
+    * Position 1 in right top square
+    * Position 2 in left bottom square
+    * Position 3 in right bottom square
