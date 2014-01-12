424802ca50fe157009d68f1163efede3a7e46f78
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-01-12 15:34:49 +0100

    Merged devel branch

---
+++ library/src/main/java/cat/alorma/capsulecorp/library/capsule/abs/BitmapCapsule.java
@@ -0,0 +1,31 @@
+package cat.alorma.capsulecorp.library.capsule.abs;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import cat.alorma.capsulecorp.library.capsule.abs.Capsule;
+
+/**
+ * Created by Bernat on 25/11/13.
+ */
+public abstract class BitmapCapsule extends Capsule {
+
+    protected BitmapCapsule() {
+    }
+
+    @Override
+    public void boom(Canvas canvas,Paint paint, Rect rect) {
+        super.boom(canvas, paint, rect);
+        Bitmap bitmap = getBitmap();
+
+        if (bitmap != null && paint != null && rect != null) {
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawBitmap(bitmap, null, rect, paint);
+        }
+    }
+
+    protected abstract Bitmap getBitmap();
+
+}
