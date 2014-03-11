b9e17df40074bf2862da96c3c4644fc8bba83792
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-03-11 21:21:37 +0100

    Drawer with titles, prepare for release

--- Demo/src/main/java/cat/alorma/capsules/ui/activity/MainActivity.java
+++ Demo/src/main/java/cat/alorma/capsules/ui/activity/MainActivity.java
@@ -8,12 +8,11 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.SparseArray;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -32,22 +31,19 @@ import cat.alorma.capsules.ui.fragment.TitleStrip;
 
 public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
 
-    private SparseArray<Fragment> fragments;
-    private SparseArray<TitleStrip> fragmentsTitles;
+    private ArrayList<Fragment> fragments;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        createFragments();
-
         ActionBar actionBar = getActionBar();
 
         if (actionBar != null) {
-
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayUseLogoEnabled(false);
@@ -76,12 +72,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
 
         List<String> objects = new ArrayList<String>();
 
-        for (int i = 0; i < fragments.size(); i++) {
-            TitleStrip t = (TitleStrip) fragments.get(fragments.keyAt(i));
-            objects.add(t.getTitle());
-        }
-
-        ArrayAdapter<String> adapter = new NavigationAdapter(this, objects);
+        adapter = new NavigationAdapter(this, objects);
 
         mListDrawer.setAdapter(adapter);
         mListDrawer.setOnItemClickListener(this);
@@ -100,20 +91,22 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
     }
 
     private void createFragments() {
-        fragments = new SparseArray<Fragment>();
-        fragmentsTitles = new SparseArray<TitleStrip>();
+        fragments = new ArrayList<Fragment>();
 
-        fragments.put(0, new ColorsFragment());
-        fragments.put(1, new TextColorsFragment());
-        fragments.put(2, new ImagesCapsulesFragment());
-        fragments.put(3, new ListCapsulesFragment());
-        fragments.put(4, new CustomFragment());
-        fragments.put(5, new AnimFragment());
-        fragments.put(6, new ContactsFragment());
+        fragments.add(new ColorsFragment());
+        fragments.add(new TextColorsFragment());
+        fragments.add(new ImagesCapsulesFragment());
+        fragments.add(new ListCapsulesFragment());
+        fragments.add(new CustomFragment());
+        fragments.add(new AnimFragment());
+        fragments.add(new ContactsFragment());
+
+        fragments.trimToSize();
 
         for (int i = 0; i < fragments.size(); i++) {
-            TitleStrip t = (TitleStrip) fragments.get(fragments.keyAt(i));
-            fragmentsTitles.put(i, t);
+            TitleStrip t = (TitleStrip) fragments.get(i);
+            Log.i("ALORMA", "Pos: " + i + " -> " + t.getTitle());
+            adapter.add(t.getTitle());
         }
     }
 
@@ -124,8 +117,10 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
         getMenuInflater().inflate(R.menu.main, menu);
 
         return true;
-    }@Override
-     public boolean onPrepareOptionsMenu(Menu menu) {
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -153,10 +148,9 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        mDrawerLayout.closeDrawers();
-
-        getActionBar().setTitle(fragmentsTitles.get(fragmentsTitles.keyAt(position)).getTitle());
-
-        setFragment(position);
+        if (position < fragments.size()) {
+            mDrawerLayout.closeDrawers();
+            setFragment(position);
+        }
     }
 }
